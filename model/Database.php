c19025eead4197026a7d51ee98dc9f1c272f52bb
Diegoalfonso Dasilva <diedasilva@etmlnet.local>
2022-04-05 14:06:38 +0200

    coucou

--- model/Database.php
+++ model/Database.php
@@ -1,38 +1,43 @@
-<!--
-ETML 
-Author : Kandasamy Pruthvin
-Date   : 28.02.2022
-Description: les données sont traitées et affichées directement dans la page d’accueil.
- Mais, nous allons créer plusieurs pages avec des liaisons à la BD. A terme, nous ne voulons pas recopier toujours les mêmes instructions.
- De ce fait, nous allons créer une classe afin de regrouper les méthodes nécessaires à nos requêtes.
--->
 <?php
+
+/**
+ * 
+ * Fichier database pour appeler les méthodes
+ * 
+ * Auteur : Diego Da Silva
+ * Date : 28.02.2022
+ * Description : 
+ */
+
+
  class Database {
+
+
     // Variable de classe
     private $connector;
-   
+
     /**
-     *  methode permettant de se connecter a la base de donnée avec PDO
+     * Se connecter via PDO et utilise la variable de classe $connector
      */
     public function __construct(){
-        $pass = '.Etml-';
-        $user = 'dbUser_pt41kyp';
+
         try
         {
-        $this->connector = new PDO('mysql:host=localhost;dbname=db_nickname_pt41kyp;charset=utf8' , $user, $pass);
+            $this -> connector = new PDO('mysql:host=localhost; dbname=db_nickname_diedasilva; charset=utf8' , 'dbUser_DiegoDaSilva', '.Etml-');
         }
         catch (PDOException $e)
         {
-        die('Erreur : ' . $e->getMessage());
+            die('Erreur : ' . $e->getMessage());
         }
-        // Se connecter via PDO et utilise la variable de classe $connector
     }
 
     /**
      * permet de préparer et d’exécuter une requête de type simple (sans where)
      */
     private function querySimpleExecute($query){
-        $req = $this -> connector->query($query);
+
+        $req = $this -> connector->prepare($query);
+        $req->execute();
         return $req;
     }
 
@@ -40,9 +45,10 @@ Description: les données sont traitées et affichées directement dans la page 
      * permet de préparer, de binder et d’exécuter une requête (select avec where ou insert, update et delete)
      */
     private function queryPrepareExecute($query, $binds){
+        
         $req =  $this->connector->prepare($query);
         foreach($binds as $key => $value){
-            $req->bindValue($value['name'], $value["value"], $value["type"]);
+            $req->bindValue($binds[$key]['name'], $binds[$key]["value"], $binds[$key]["type"]);
         }
         $req->execute();
 
@@ -53,6 +59,7 @@ Description: les données sont traitées et affichées directement dans la page 
      * traiter les données pour les retourner par exemple en tableau associatif (avec PDO::FETCH_ASSOC)
      */
     private function formatData($req){
+
         $result = $req->fetchALL(PDO::FETCH_ASSOC);
         return $result;
     }
@@ -61,244 +68,130 @@ Description: les données sont traitées et affichées directement dans la page 
      * vider le jeu d’enregistrement
      */
     private function unsetData($req){
+
         $req->closeCursor();
     }
 
     /**
-     * methode permettant de récupèrer tout les enseignants
+     * récupère la liste de tous les enseignants de la BD    
      */
     public function getAllTeachers(){
-        //récupère la liste de tous les enseignants de la BD
-        //avoir la requête sql
-        //appeler la méthode pour executer la requête
-        //appeler la méthode pour avoir le résultat sous forme de tableau
-        //retour tous les enseignants
-        $queryTeachers = "SELECT * FROM t_teacher ";
-        $reqTeachers = $this->querySimpleExecute($queryTeachers);
-         
-        $returnTeachers=$this->formatData($reqTeachers);
-        $this -> unsetData($reqTeachers);
-        return $returnTeachers;
+    
+        // avoir la requête sql
+        $queryTeacher = 'SELECT idTeacher, `teaFirstname`, `teaName`, `teaGender`, `teaNickname` FROM `t_teacher`';
+
+        // appeler la méthode pour executer la requête
+        $getQueryTeacher = $this -> querySimpleExecute($queryTeacher);
+
+        // appeler la méthode pour avoir le résultat sous forme de tableau
+        // retour tous les enseignants
+        return $this -> formatData($getQueryTeacher);
+        
     }
 
     /**
-     * methode permettant de récupèrer un enseignant
+     * récupère la liste des informations pour 1 enseignant
      */
     public function getOneTeacher($id){
-        // récupère la liste des informations pour 1 enseignant b 
+
         // avoir la requête sql pour 1 enseignant (utilisation de l'id)
-        // appeler la méthode pour executer la requête
-        // appeler la méthode pour avoir le résultat sous forme de tableau
-        // retour l'enseignant
-        $queryOneTeacher = "SELECT * FROM t_teacher INNER JOIN t_section ON t_section.idSection = t_teacher.fkSection WHERE idTeacher=:varId";
+        $queryOneTeacher = "SELECT `teaFirstname`, `teaName`, `teaGender`, `teaNickname`, `teaOrigine`, `secName` FROM `t_teacher` INNER JOIN `t_section` ON t_section.idSection = t_teacher.fkSection WHERE idTeacher=:varId";
+
         $bindTeacher = array(
             array("name" => "varId" , "value" => $id, "type"=> PDO::PARAM_INT)
         );
-        $reqTeachers = $this->queryPrepareExecute($queryOneTeacher,$bindTeacher);
-        $returnTeachers=$this->formatData($reqTeachers);
-        $this -> unsetData($reqTeachers);
-        return $returnTeachers;
-    }
 
-    /**
-     * methode permettant d'ajouter un enseignant
-     */
-    public function InsertTeacher($teacherData)
-    {
-        // insert les informations
-        // avoir la requête sql
         // appeler la méthode pour executer la requête
-        $query = "INSERT INTO t_teacher (idTeacher, teaFirstname, teaName, teaGender, teaNickname, teaOrigine, fkSection) 
-                  VALUES (NULL, :firstName, :name, :genre, :nickName, :origin, :section)";
+        $getQueryOneTeacher = $this -> queryPrepareExecute($queryOneTeacher, $bindTeacher);
 
-        $binds = [
-            ["name" => 'name', 'value' => $teacherData['name'], 'type' => PDO::PARAM_STR],
-            ["name" => 'firstName', 'value' => $teacherData['firstName'], 'type' => PDO::PARAM_STR],
-            ["name" => 'genre', 'value' => $teacherData['genre'], 'type' => PDO::PARAM_STR],
-            ["name" => 'nickName', 'value' => $teacherData['nickName'], 'type' => PDO::PARAM_STR],
-            ["name" => 'origin', 'value' => $teacherData['origin'], 'type' => PDO::PARAM_STR],
-            ["name" => 'section', 'value' => $teacherData['section'], 'type' => PDO::PARAM_INT]
-        ];
-
-        $this->queryPrepareExecute($query, $binds);
+        // appeler la méthode pour avoir le résultat sous forme de tableau
+        // retour l'enseignant
+        return $this -> formatData($getQueryOneTeacher);    
     }
-
-     /**
-     * methode permettant de modifier un enseignant
-     */
-    public function modifyTeacher($teacherData)
-    {
-        //modifie les informations du teacher
-        //avoir la requête sql
-        // appeler la méthode pour executer la requête.
-        $query = "UPDATE t_teacher SET teaFirstname = :firstName, teaName = :name, teaGender = :genre, teaNickname = :nickName,
-                     teaOrigine = :origin, fkSection = :section WHERE t_teacher.idTeacher = :id";
-
-        $binds = [
-            ["name" => 'name', 'value' => $teacherData['name'], 'type' => PDO::PARAM_STR],
-            ["name" => 'firstName', 'value' => $teacherData['firstName'], 'type' => PDO::PARAM_STR],
-             ["name" => 'genre', 'value' => $teacherData['genre'], 'type' => PDO::PARAM_STR],
-             ["name" => 'nickName', 'value' => $teacherData['nickName'], 'type' => PDO::PARAM_STR],
-             ["name" => 'origin', 'value' => $teacherData['origin'], 'type' => PDO::PARAM_STR],
-             ["name" => 'section', 'value' => $teacherData['section'], 'type' => PDO::PARAM_INT],
-             ["name" => 'id', 'value' => $teacherData['id'], 'type' => PDO::PARAM_INT]
-        ];
-
-        $this->queryPrepareExecute($query, $binds);
-    }
-
     /**
-     * methode permettant de delete un enseignant
+     * créer un enseignant avec toute ses coordenées
      */
-    public function deleteTeacher($idTeacher)
-    {
-        //supprime l'enseignant
-        //avoir la requête sql 
-        //appeler la méthode pour executer la requête
-        //appeler la méthode pour avoir le résultat sous forme de tableau.
-        $query = 'DELETE FROM t_teacher WHERE idTeacher = :idTeacher';
+    public function insertTeacher($firstname, $name, $gender, $nickname, $origin, $section){
+        
+        //avoir la requête pour insérer un professeur
+            $queryInsertTeacher = "INSERT INTO `t_teacher`(`idTeacher`, `teaFirstname`, `teaName`, `teaGender`, `teaNickname`, `teaOrigine`, `fkSection`) VALUES (NULL, :firstname, :name, :gender, :nickname, :origin, :section)";
 
-        //avoir la requête sql pour le delete.
-        $binds = [
-            ["name" => "idTeacher", "value" => $idTeacher, "type" => PDO::PARAM_INT]
-        ];
-        $req = $this->queryPrepareExecute($query, $binds);
-
-        return $this->formatData($req);
+            $bindInsertTeacher = array(
+                0=>array("name" => "firstname", "value" => $firstname, "type" => PDO::PARAM_STR),
+                1=>array("name" => "name", "value" => $name, "type" => PDO::PARAM_STR),
+                2=>array("name" => "gender", "value" => $gender, "type" => PDO::PARAM_STR),
+                3=>array("name" => "nickname", "value" => $nickname, "type" => PDO::PARAM_STR),
+                4=>array("name" => "origin", "value" => $origin, "type" => PDO::PARAM_STR),
+                5=>array("name" => "section", "value" => $section, "type" => PDO::PARAM_INT)
+            );
+            
+            // appeler la méthode pour executer la requête
+            $this->queryPrepareExecute($queryInsertTeacher, $bindInsertTeacher);
     }
-
     /**
-     * methode permettant de récupérer les sections
+     * Fontion pour afficher toute les sections
      */
-    public function getSections()
+    public function getAllSections()
     {
-        //récupère la liste de toutes les section de la BD
-        //appeler la méthode pour executer la requête 
-        $query = 'SELECT * FROM t_section';
-        $req = $this->querySimpleExecute($query);
+        //requête pour selectionner les sections
+        $querySections='SELECT `idSection`, `secName` FROM `t_section`';
 
-        // Retour les sections sous forme de tableau associatif
-        return $this->formatData($req);
-    }
+        // appeler la méthode pour executer la requête
+        $result=$this->querySimpleExecute($querySections);
     
-    /**
-     * methode permettant de récupérer une sections
-     */
-    public function getOneSections()
-    {
-        //récupère la liste de toutes les section de la BD
-        //avoir la requête sql
-        //appeler la méthode pour executer la requête
-        $query = 'SELECT * FROM t_section';
-        $binds = [
-            ["name" => "idSection", "value" => $idSection, "type" => PDO::PARAM_INT]
-        ];
-        $req = $this->queryPrepareExecute($query);
-
-        // Retour les sections sous forme de tableau associatif
-        return $this->formatData($req);
+        // appeler la méthode pour avoir le résultat sous forme de tableau
+        // retour tous les enseignants
+        return $this->formatData($result);
     }
-
-    /**
-     * methode permettant de récupérer un utilisateur
-     */
-    public function getOneUser($username,$password)
+     /**
+      * Permet de modifier un enseignant qui est déjà enregistrer
+      */
+    public function updateTeacher($id, $firstName,  $name,   $gender, $nickname, $origine, $section, )
     {
-        //récupère un utilisateur de la BD
-        //avoir la requête sql
-        //appeler la méthode pour executer la requête
-        $query = 'SELECT * FROM t_user WHERE useLogin = :username AND usePassword = :password';
-        $binds = [
-            ["name" => "username", "value" => $username, "type" => PDO::PARAM_STR],
-            ["name" => "password", "value" => $password, "type" => PDO::PARAM_STR]
-        ];
-        $req = $this->queryPrepareExecute($query, $binds);
+        //requête pour modifier 
+        $addTeacher = "UPDATE t_teacher SET teaFirstname=:firstname, teaName=:name, teaGender=:gender, teaNickname=:nickname, teaOrigine=:origine, fkSection=:section WHERE idTeacher=:id";
 
-        // Retour les sections sous forme de tableau associatif
-        return $this->formatData($req);
+        //tableau pour toute les informations
+        $bindTeacher = array(
+            0 => array("name" => "firstname", "value" => $firstName, "type" => PDO::PARAM_STR),
+            1 => array("name" => "name", "value" => $name, "type" => PDO::PARAM_STR),
+            2 => array("name" => "gender", "value" => $gender, "type" => PDO::PARAM_STR),
+            3 => array("name" => "nickname", "value" => $nickname, "type" => PDO::PARAM_STR),
+            4 => array("name" => "origine", "value" => $origine, "type" => PDO::PARAM_STR),
+            5 => array("name" => "section", "value" => $section, "type" => PDO::PARAM_INT),
+            6 => array("name" => "id", "value" => $id, "type" => PDO::PARAM_INT)
+        );
+         //Appeler la méthode pour executer la requète
+         $addTeacher = $this->queryPrepareExecute($addTeacher, $bindTeacher);
 
+        // appeler la méthode pour avoir le résultat sous forme de tableau
+        // retour tous les enseignants
+         $returnTeacher = $this->formatData($addTeacher);
+
+         //Vide le jeu d'enregistrement 
+         $this->unsetData($addTeacher);
+
+         //Retourne l'enseignant
+         return $returnTeacher;
     }
 
     /**
-     * methode permettant de récupérer tout les utilisateurs
+     * Permet de supprimer un professeur
      */
-    public function getUser($idUser){
-        //récupère la liste de tous les utilisateur de la BD
-        //avoir la requête sql
-        //appeler la méthode pour executer la requête
-        $query = 'SELECT * FROM t_user WHERE idUser = :idUser';
-        $binds = [
-            ["name" => "idUser","value" => $idUser, "type" => PDO::PARAM_INT]
-        ];
-        $req = $this->queryPrepareExecute($query, $binds);
+    public function deleteTeacher($id){
 
-        // Retour les sections sous forme de tableau associatif
-        return $this->formatData($req);
+        //requête pour modifier
+        $queryTeacher ="DELETE FROM `t_teacher` WHERE idTeacher =:varId";
+ 
+        //tableau
+        $bindTeachers = array( 
+             array("name" => "varId", "value" => $id, "type" => PDO::PARAM_INT)
+        );
 
+        //Appel la méthode pour exécuter la requête
+        $this->queryPrepareExecute($queryTeacher, $bindTeachers);
     }
-    /** 
-     * methode permettant d'ajoute une session dans la base de données
-     */
-    public function addSession($idUser)
-    {
-        //insert une session a la BD
-        //avoir la requête sql
-        //appeler la méthode pour executer la requête
-        $query = "INSERT INTO t_session (idSession, fkUser) VALUES (NULL, :idUser)";
-        $binds =  [
-            ["name" => 'idUser', 'value' => $idUser, 'type' => PDO::PARAM_STR]
-        ];
-
-        $this->queryPrepareExecute($query, $binds);
-
-        // Retourne l'id de la session crée
-        return $this->getIdSessionByUserId($idUser);
+    public function addVote(){
     }
-
-    /*
-     * methode permettant de récupèrer l'ID d'une session en fonction d'un idUser ; Retourne null si pas de résultats ; sinon retourne l'idSession
-     */
-    private function getIdSessionByUserId($idUser)
-    {
-        $query = "SELECT idSession FROM t_session WHERE fkUser = :idUser";
-        $binds = [
-            ["name" => 'idUser', 'value' => $idUser, 'type' => PDO::PARAM_STR]
-        ];
-
-        $req = $this->queryPrepareExecute($query, $binds);
-        $result = $this->formatData($req);
-
-        return $result ? $result[0]['idSession'] : null;
-    }
-
-    /**
-     * methode permettant de Récupèrer une session avec l'id donné et si elle existe retoune la session sinon null
-     */
-    public function getOneSession($idSession)
-    {
-        $query = "SELECT * FROM t_session WHERE idSession = :idSession";
-        $binds = [
-            ["name" => 'idSession','value' => $idSession, 'type' => PDO::PARAM_INT]
-        ];
-
-        $req = $this->queryPrepareExecute($query, $binds);
-        $session = $this->formatData($req);
-
-        return !$session ? null : $session[0];
-    }
-
-    /**
-    * methode permettant de supprimer une session avec l'id donné
-    */ 
-    public function deleteSession($idSession)
-    {
-        return !$session ? null : $session[0];
-        $query = 'DELETE FROM t_session WHERE idSession = :idSession';
-        $binds = [
-            ["name" => "idSession", "value" => $idSession, "type" => PDO::PARAM_INT]
-        ];
-        $req = $this->queryPrepareExecute($query, $binds);
-        return $this->formatData($req);
-    }
- }
+}
 ?>
