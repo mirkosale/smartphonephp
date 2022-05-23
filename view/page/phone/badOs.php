ccc47877e42dc1aa898fb1e259aa420d6f3f925b
Mirko Sale <pb51vab@etmlnet.local>
2022-05-23 10:50:23 +0200

    Modified error message in the list os page

--- view/page/phone/badOs.php
+++ view/page/phone/badOs.php
@@ -1,7 +1,7 @@
 <div class="container">
 	<div class="row">
 		<br>
-		<p>Cet OS n'existe pas.</p>
+		<p>Il n'y pas de smartphone utilisant cet OS.</p>
 		<a href="index.php?controller=phone&action=orderOS">Retour à la page de liste</a>
 		<br>
 	</div>
